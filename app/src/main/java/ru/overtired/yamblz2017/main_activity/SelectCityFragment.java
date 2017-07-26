8cafd8c993599c0f0e2d105e13cb5d5cb4d2efda
KarapetianDV <karapetiandav@gmail.com>
2017-07-26 14:57:19 +0300

    Added: Preference onClickListener

--- app/src/main/java/ru/overtired/yamblz2017/main_activity/SelectCityFragment.java
+++ app/src/main/java/ru/overtired/yamblz2017/main_activity/SelectCityFragment.java
@@ -2,14 +2,14 @@ package ru.overtired.yamblz2017.main_activity;
 
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import android.support.v4.app.DialogFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import ru.overtired.yamblz2017.R;
 
-public class SelectCityFragment extends Fragment {
+public class SelectCityFragment extends DialogFragment {
 
     public SelectCityFragment() {
         // Required empty public constructor
