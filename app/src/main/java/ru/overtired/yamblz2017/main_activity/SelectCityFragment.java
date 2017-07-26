1f762bcf673acb5055c22e18f10dbca48228f487
KarapetianDV <karapetiandav@gmail.com>
2017-07-26 15:41:56 +0300

    Change: Select City Fragment layout

--- app/src/main/java/ru/overtired/yamblz2017/main_activity/SelectCityFragment.java
+++ app/src/main/java/ru/overtired/yamblz2017/main_activity/SelectCityFragment.java
@@ -1,20 +1,32 @@
 package ru.overtired.yamblz2017.main_activity;
 
 
+import android.app.Dialog;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ListView;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import ru.overtired.yamblz2017.R;
 
 public class SelectCityFragment extends DialogFragment {
 
+    @BindView(R.id.enter_city_edittext)
+    EditText cityEditText;
+
+    @BindView(R.id.city_suggestion_list)
+    ListView suggestionsListView;
+
+    private Dialog dialog;
+
     public SelectCityFragment() {
         // Required empty public constructor
     }
-
 
     public static SelectCityFragment newInstance() {
         return new SelectCityFragment();
@@ -22,7 +34,19 @@ public class SelectCityFragment extends DialogFragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_select_city, container, false);
+        View view = inflater.inflate(R.layout.fragment_select_city, container, false);
+        ButterKnife.bind(this, view);
+
+        return view;
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        dialog = getDialog();
+        if (dialog != null) {
+            dialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        }
+    }
 }
